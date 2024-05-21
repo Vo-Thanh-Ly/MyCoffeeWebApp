d14fceaf8f7d2b979632ad283a3331ea5c8920fd
Vo Thanh Ly <vothanhly632002@gmail.com>
2024-05-21 23:51:19 +0700

    21/5/2024

---
+++ MyCoffeeWebApp/MyCoffeeWebApp/Controllers/ALCOHOL_COLLECTS_MONEYController.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MyCoffeeWebApp;
+
+namespace MyCoffeeWebApp.Controllers
+{
+    public class ALCOHOL_COLLECTS_MONEYController : Controller
+    {
+        private MyCoffeeWebAppEntities db = new MyCoffeeWebAppEntities();
+
+        // GET: ALCOHOL_COLLECTS_MONEY
+        public async Task<ActionResult> Index()
+        {
+            var aLCOHOL_COLLECTS_MONEY = db.ALCOHOL_COLLECTS_MONEY.Include(a => a.DATE).Include(a => a.TYPE_OF_WINE);
+            return View(await aLCOHOL_COLLECTS_MONEY.ToListAsync());
+        }
+
+        // GET: ALCOHOL_COLLECTS_MONEY/Details/5
+        public async Task<ActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ALCOHOL_COLLECTS_MONEY aLCOHOL_COLLECTS_MONEY = await db.ALCOHOL_COLLECTS_MONEY.FindAsync(id);
+            if (aLCOHOL_COLLECTS_MONEY == null)
+            {
+                return HttpNotFound();
+            }
+            return View(aLCOHOL_COLLECTS_MONEY);
+        }
+
+        // GET: ALCOHOL_COLLECTS_MONEY/Create
+        public ActionResult Create()
+        {
+            ViewBag.DATE_ID = new SelectList(db.DATEs, "DATE_ID", "date_name");
+            ViewBag.TYPE_OF_WINE_ID = new SelectList(db.TYPE_OF_WINE, "TYPE_OF_WINE_ID", "TYPE_OF_WINE_NAME");
+            return View();
+        }
+
+        // POST: ALCOHOL_COLLECTS_MONEY/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "ALCOHOL_COLLECTS_MONEY_QUANTITY,DATE_ID,TYPE_OF_WINE_ID,ALCOHOL_COLLECTS_MONEY_PRICE,ALCOHOL_COLLECTS_MONEY_ID")] ALCOHOL_COLLECTS_MONEY aLCOHOL_COLLECTS_MONEY)
+        {
+            if (ModelState.IsValid)
+            {
+                db.ALCOHOL_COLLECTS_MONEY.Add(aLCOHOL_COLLECTS_MONEY);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.DATE_ID = new SelectList(db.DATEs, "DATE_ID", "date_name", aLCOHOL_COLLECTS_MONEY.DATE_ID);
+            ViewBag.TYPE_OF_WINE_ID = new SelectList(db.TYPE_OF_WINE, "TYPE_OF_WINE_ID", "TYPE_OF_WINE_NAME", aLCOHOL_COLLECTS_MONEY.TYPE_OF_WINE_ID);
+            return View(aLCOHOL_COLLECTS_MONEY);
+        }
+
+        // GET: ALCOHOL_COLLECTS_MONEY/Edit/5
+        public async Task<ActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ALCOHOL_COLLECTS_MONEY aLCOHOL_COLLECTS_MONEY = await db.ALCOHOL_COLLECTS_MONEY.FindAsync(id);
+            if (aLCOHOL_COLLECTS_MONEY == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.DATE_ID = new SelectList(db.DATEs, "DATE_ID", "date_name", aLCOHOL_COLLECTS_MONEY.DATE_ID);
+            ViewBag.TYPE_OF_WINE_ID = new SelectList(db.TYPE_OF_WINE, "TYPE_OF_WINE_ID", "TYPE_OF_WINE_NAME", aLCOHOL_COLLECTS_MONEY.TYPE_OF_WINE_ID);
+            return View(aLCOHOL_COLLECTS_MONEY);
+        }
+
+        // POST: ALCOHOL_COLLECTS_MONEY/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "ALCOHOL_COLLECTS_MONEY_QUANTITY,DATE_ID,TYPE_OF_WINE_ID,ALCOHOL_COLLECTS_MONEY_PRICE,ALCOHOL_COLLECTS_MONEY_ID")] ALCOHOL_COLLECTS_MONEY aLCOHOL_COLLECTS_MONEY)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(aLCOHOL_COLLECTS_MONEY).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            ViewBag.DATE_ID = new SelectList(db.DATEs, "DATE_ID", "date_name", aLCOHOL_COLLECTS_MONEY.DATE_ID);
+            ViewBag.TYPE_OF_WINE_ID = new SelectList(db.TYPE_OF_WINE, "TYPE_OF_WINE_ID", "TYPE_OF_WINE_NAME", aLCOHOL_COLLECTS_MONEY.TYPE_OF_WINE_ID);
+            return View(aLCOHOL_COLLECTS_MONEY);
+        }
+
+        // GET: ALCOHOL_COLLECTS_MONEY/Delete/5
+        public async Task<ActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ALCOHOL_COLLECTS_MONEY aLCOHOL_COLLECTS_MONEY = await db.ALCOHOL_COLLECTS_MONEY.FindAsync(id);
+            if (aLCOHOL_COLLECTS_MONEY == null)
+            {
+                return HttpNotFound();
+            }
+            return View(aLCOHOL_COLLECTS_MONEY);
+        }
+
+        // POST: ALCOHOL_COLLECTS_MONEY/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(string id)
+        {
+            ALCOHOL_COLLECTS_MONEY aLCOHOL_COLLECTS_MONEY = await db.ALCOHOL_COLLECTS_MONEY.FindAsync(id);
+            db.ALCOHOL_COLLECTS_MONEY.Remove(aLCOHOL_COLLECTS_MONEY);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ MyCoffeeWebApp/MyCoffeeWebApp/Controllers/CharController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyCoffeeWebApp.Controllers
+{
+    public class CharController : Controller
+    {
+        // GET: Char
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Char/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Char/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Char/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Char/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Char/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Char/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Char/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
